df7b84fd089f84db07e52b151fbff56ddd275b3d
ebbevang <ev@evang.dk>
2016-09-30 10:51:40 +0200

    adds ignore file

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        bool AddStudent(Student student);
+
+        [OperationContract]
+        bool RemoveStudent(Student student);
+
+        [OperationContract]
+        List<Student> getStudentByFirstName(String firstname);
+
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Student
+    {
+        
+        private string _lastName;
+        private string _firstName;
+        private string _cpr;
+
+        [DataMember]
+        public string Cpr
+        {
+            get { return _cpr; }
+            set { _cpr = value; }
+        }
+
+        [DataMember]
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        [DataMember]
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj.GetType() != typeof(Student))
+            {
+                return false;
+            }
+            else // Nu ved jeg at det er et student obj
+            {
+                Student studentObj = (Student) obj;
+                if (_cpr.Equals(studentObj.Cpr)) //hvis cpr er ens er objekterne ens!
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private static List<Student> students = new List<Student>();
+
+        public bool AddStudent(Student student)
+        {
+            //findes studerende allerede?
+            foreach (var s in students)
+            {
+                //hvios studerende findes
+                if (s.Equals(student)) return false;
+            }
+
+            //studerende findes ikke....
+            students.Add(student);
+            return true;
+        }
+
+        public bool RemoveStudent(Student student)
+        {
+            foreach (var s in students)
+            {
+                if (s.Equals(student))
+                {
+                    students.Remove(student);
+                    return true;
+                }
+     
+            }
+            return false;
+        }
+
+        public List<Student> getStudentByFirstName(string firstname)
+        {
+            List<Student> toBeReturnedList = new List<Student>();
+            foreach (var student in students)
+            {
+                if (student.FirstName.ToLower().Equals(firstname.ToLower()))
+                {
+                    toBeReturnedList.Add(student);
+                }
+            }
+            return toBeReturnedList;
+        }
+    }
+}
